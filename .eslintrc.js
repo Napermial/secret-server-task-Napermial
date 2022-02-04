ab5468ea0eb5e3314a85e3bf6c95d66f86259841
Napermial <hatvani9823@gmail.com>
2022-02-04 11:26:14 +0100

    hashhing added with mock data storage

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser',
+    ecmaVersion: "latest",
+    requireConfigFile:false
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  plugins: ['jest'],
+  rules: {
+  }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,27 +1,45 @@
 const { Router } = require('express')
+const { Keccak } = require('sha3')
 
 const router = Router()
 
-// Mock Users
-const users = [
-  { name: 'Alexandre' },
-  { name: 'Pooya' },
-  { name: 'Sébastien' }
+// mock secrets
+const secrets = [
+  {
+    hash: '[The hash of the string]',
+    secretText: '[The original text]',
+    createdAt: '[The Timestamp when the secret was created]',
+    expiresAt: '[The Timestamp of the secret if TTL is given]',
+    remainingViews: 0
+  }
 ]
 
-/* GET users listing. */
-router.get('/users', function (req, res, next) {
-  res.json(users)
+router.get('/api/secret/:hash', function (req, res, next) {
+  res.json(secrets)
 })
 
-/* GET user by ID. */
-router.get('/users/:id', function (req, res, next) {
-  const id = parseInt(req.params.id)
-  if (id >= 0 && id < users.length) {
-    res.json(users[id])
-  } else {
-    res.sendStatus(404)
+router.post('/api/secret/', function (req, res) {
+  const secretBody = req.body
+  if (!('secret' in secretBody) || !('expireAfterViews' in secretBody) || !('expireAfter' in secretBody)) {
+    return res.error()
   }
+  const keccak = new Keccak(256)
+  const now = new Date()
+  let expiresAtTime = new Date()
+
+  if (secretBody.expireAfter !== undefined) {
+    expiresAtTime = new Date(now.getTime() + parseInt(secretBody.expireAfter) * 60000)
+  }
+
+  const addSecret = {
+    hash: keccak.update(secretBody.secret, 'utf-8'),
+    secretText: secretBody.secret,
+    createdAt: now.toISOString(),
+    expiresAt: expiresAtTime,
+    remainingViews: secretBody.expireAfterViews
+  }
+
+  secrets.push(addSecret)
 })
 
 module.exports = router
